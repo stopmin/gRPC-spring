12015f524c77e0b08e14fe830ae718d4218ff5cf
stopmin <geemin0@naver.com>
2024-03-08 14:17:36 +0900

    grpc server 설정

--- grpc-server/src/main/kotlin/example/grpcserver/GrpcServerService.kt
+++ grpc-server/src/main/kotlin/example/grpcserver/GrpcServerService.kt
@@ -1,15 +1,15 @@
 package example.grpcserver
 
+import helloworld.HelloReply
+import helloworld.HelloRequest
+import helloworld.SimpleGrpc
 import io.grpc.stub.StreamObserver
 import net.devh.boot.grpc.server.service.GrpcService
-import org.chb.examples.lib.HelloReply
-import org.chb.examples.lib.HelloRequest
-import org.chb.examples.lib.SimpleGrpc
 
 @GrpcService
 class GrpcServerService : SimpleGrpc.SimpleImplBase() {
     override fun sayHello(request: HelloRequest, responseObserver: StreamObserver<HelloReply>) {
-        val reply = HelloReply.newBuilder().setMessage(request.name+"이 도착했습니닷").build()
+        val reply = HelloReply.newBuilder().setMessage(request.name + "이 도착했습니닷").build()
         println(request.name)
         responseObserver.onNext(reply)
         responseObserver.onCompleted()
