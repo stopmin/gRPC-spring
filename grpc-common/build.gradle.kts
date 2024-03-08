bf3c840f250e4790a045d49fafea79416520579b
stopmin <geemin0@naver.com>
2024-03-08 14:22:07 +0900

    spring gRPC 테스트 성공

---
+++ grpc-common/build.gradle.kts
@@ -0,0 +1,91 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
+
+plugins {
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+
+    id("com.google.protobuf") version "0.9.4" // protobuf
+}
+
+group = "example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+val grpcVersion = "3.25.1"
+val grpcKotlinVersion = "1.2.1"
+val grpcProtoVersion = "1.61.1"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+
+    // grpc
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcProtoVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$grpcVersion")
+
+    // grpc-spring-boot-starter
+    implementation("net.devh:grpc-spring-boot-starter:3.0.0.RELEASE")
+
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType < KotlinCompile > {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType < Test > {
+    useJUnitPlatform()
+}
+
+sourceSets {
+    getByName("main") {
+        java {
+            srcDirs(
+                    "build/generated/source/proto/main/java",
+                    "build/generated/source/proto/main/kotlin"
+            )
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$grpcVersion"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcProtoVersion"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk7@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}
