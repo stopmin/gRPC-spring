61596ec37e8d5aa07da0005fcb9ef6beb1d92084
stopmin <geemin0@naver.com>
2024-04-11 23:47:39 +0900

    feat: WebConfig.kt 설정

---
+++ grpc-server/src/main/kotlin/example/grpcserver/config/WebConfig.kt
@@ -0,0 +1,19 @@
+package example.grpcserver.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
+    }
+}
+
