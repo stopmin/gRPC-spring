c43d437d85c53a33a141516c880543475c7236b3
stopmin <geemin0@naver.com>
2024-04-11 23:47:28 +0900

    feat: Grpc Server 예제

--- grpc-server/src/main/kotlin/example/grpcserver/GrpcServerApplication.kt
+++ grpc-server/src/main/kotlin/example/grpcserver/GrpcServerApplication.kt
@@ -1,5 +1,9 @@
 package example.grpcserver
 
+import example.grpcserver.echo.EchoService
+import example.grpcserver.echo.HelloGrpcServer
+import io.grpc.Server
+import io.grpc.ServerBuilder
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +12,10 @@ class GrpcServerApplication
 
 fun main(args: Array<String>) {
     runApplication<GrpcServerApplication>(*args)
+    val server: Server = ServerBuilder.forPort(8980).addService(EchoService())
+        .build()
+    val truckerLocationServer = HelloGrpcServer(8980, server)
+    truckerLocationServer.start()
+    truckerLocationServer.blockUntilShutdown()
+    truckerLocationServer.stop()
 }
